55fa9fc9b18d62bfaff7640262319733a00445e2
Charles Culver <charlie.culver@gmail.com>
2022-04-05 20:13:09 -0400

    fixing kill script

--- kill.sh
+++ kill.sh
@@ -1,6 +1,6 @@
-PID1 = $(cat running.json | jq -r ".GETH1")
-PID2 = $(cat running.json | jq -r ".GETH2")
-PID3 = $(cat running.json | jq -r ".GETH3")
+PID1=$(cat running.json | jq -r ".GETH1")
+PID2=$(cat running.json | jq -r ".GETH2")
+PID3=$(cat running.json | jq -r ".GETH3")
 
 kill -9 $PID1
 kill -9 $PID2
